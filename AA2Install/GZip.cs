cb7711bd45f92bc330ebb26aff93146dce585395
Patrick Bateman <roflcopter64bit@gmail.com>
2015-12-23 01:20:25 +1100

    GZip compressed config files

---
+++ AA2Install/GZip.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AA2Install
+{
+    public static class GZip
+    {
+        /// <summary>
+        /// Compresses a string in the UTF-8 format.
+        /// </summary>
+        /// <param name="str">String to compress.</param>
+        /// <returns>Byte array of compressed contents.</returns>
+        public static byte[] CompressString(string str)
+        {
+            var bytes = Encoding.UTF8.GetBytes(str);
+
+            using (var msi = new MemoryStream(bytes))
+                using (var mso = new MemoryStream())
+                {
+                    using (var gs = new GZipStream(mso, CompressionMode.Compress))
+                        msi.CopyTo(gs);
+
+                    return mso.ToArray();
+                }
+        }
+
+        /// <summary>
+        /// Decompresses a string in the UTF-8 format.
+        /// </summary>
+        /// <param name="bytes">Array of compressed contents.</param>
+        /// <returns>Decompressed string.</returns>
+        public static string DecompressString(byte[] bytes)
+        {
+            using (var msi = new MemoryStream(bytes))
+                using (var mso = new MemoryStream())
+                {
+                    using (var gs = new GZipStream(msi, CompressionMode.Decompress))
+                        gs.CopyTo(mso);
+
+                    return Encoding.UTF8.GetString(mso.ToArray());
+                }
+        }
+    }
+}
