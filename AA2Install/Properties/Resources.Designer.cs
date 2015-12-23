aa720f95c0d108834d8ba132e15739bea8990228
Bepis <36346617+bbepis@users.noreply.github.com>
2015-12-23 21:23:42 +1100

    Added a new about dialog

--- AA2Install/Properties/Resources.Designer.cs
+++ AA2Install/Properties/Resources.Designer.cs
@@ -79,5 +79,15 @@ namespace AA2Install.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap gears {
+            get {
+                object obj = ResourceManager.GetObject("gears", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
