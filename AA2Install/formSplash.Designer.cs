9b4af925cf98b94b27973ceddc10aeb15e3625af
Bepis <36346617+bbepis@users.noreply.github.com>
2015-12-24 19:02:52 +1100

    Added a splash screen

---
+++ AA2Install/formSplash.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace AA2Install
+{
+    partial class formSplash
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(formSplash));
+            this.prgLoad = new System.Windows.Forms.ProgressBar();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblVer = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // prgLoad
+            // 
+            this.prgLoad.Location = new System.Drawing.Point(5, 211);
+            this.prgLoad.Name = "prgLoad";
+            this.prgLoad.Size = new System.Drawing.Size(291, 11);
+            this.prgLoad.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.prgLoad.TabIndex = 0;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Location = new System.Drawing.Point(5, 193);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(291, 15);
+            this.lblStatus.TabIndex = 1;
+            this.lblStatus.Text = "Loading...";
+            this.lblStatus.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pictureBox1.Image = global::AA2Install.Properties.Resources.aa2install_icon;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 168);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblVer
+            // 
+            this.lblVer.Location = new System.Drawing.Point(5, 173);
+            this.lblVer.Name = "lblVer";
+            this.lblVer.Size = new System.Drawing.Size(291, 14);
+            this.lblVer.TabIndex = 3;
+            this.lblVer.Text = "AA2Install vX";
+            this.lblVer.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // formSplash
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(300, 228);
+            this.ControlBox = false;
+            this.Controls.Add(this.lblVer);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.prgLoad);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "formSplash";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AA2Install";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.ProgressBar prgLoad;
+        public System.Windows.Forms.Label lblStatus;
+        public System.Windows.Forms.PictureBox pictureBox1;
+        public System.Windows.Forms.Label lblVer;
+    }
+}
---
+++ AA2Install/formSplash.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AA2Install
+{
+    public partial class formSplash : Form
+    {
+        public formSplash()
+        {
+            InitializeComponent();
+        }
+    }
+}
